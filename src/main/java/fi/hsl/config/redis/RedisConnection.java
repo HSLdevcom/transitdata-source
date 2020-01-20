2b7f8b3b6be7c87b0d8ab912bd8601de74b7f837
Aki Väliaho <aki.valiaho@hsl.fi>
2020-01-20 15:21:27 +0200

    Lettuce pooling for fault tolerant redis connections

--- src/main/java/fi/hsl/config/redis/RedisConnection.java
+++ src/main/java/fi/hsl/config/redis/RedisConnection.java
@@ -1,14 +1,82 @@
 package fi.hsl.config.redis;
 
-import org.apache.pulsar.shade.org.apache.commons.lang.NotImplementedException;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConnection {
-    @PostConstruct
-    public void init() {
-        throw new NotImplementedException();
+    @Value("${redis.cache.hostname}")
+    private String redisHost;
+
+    @Value("${redis.cache.port}")
+    private Integer redisPort;
+
+    @Value("${redis.cache.thread.maxActive}")
+    private Integer MAXACTIVE;
+
+    @Value("${redis.cache.thread.maxIdle}")
+    private Integer MAXIDLE;
+
+    @Value("${taskScheduler.defaultLockMaxDurationMinutes}")
+    private int lockMaxDuration;
+
+    @Value("${app.environment}")
+    private String ENV;
+
+    @Value("${taskScheduler.poolSize}")
+    private int tasksPoolSize;
+
+
+    @Bean
+    public RedisConnectionFactory connectionFactory(RedisStandaloneConfiguration redisStandaloneConfiguration,
+                                                    LettucePoolingClientConfiguration lettucePoolConfig) {
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, lettucePoolConfig);
     }
+
+    @Bean
+    public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+        return new RedisStandaloneConfiguration(redisHost, redisPort);
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisTemplate<>() {{
+            setConnectionFactory(redisConnectionFactory);
+        }};
+    }
+
+    @Bean
+    LettucePoolingClientConfiguration lettucePoolConfig() {
+        return LettucePoolingClientConfiguration.builder()
+                .poolConfig(new GenericObjectPoolConfig())
+                .clientOptions(clientOptions())
+                .clientResources(clientResources())
+                .build();
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    ClientResources clientResources() {
+        return DefaultClientResources.create();
+    }
+
+    @Bean
+    ClientOptions clientOptions() {
+        return ClientOptions.builder()
+                .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+                .autoReconnect(true)
+                .build();
+    }
+
 }
